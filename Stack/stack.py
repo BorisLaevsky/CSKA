0dc2279bdd825014b4d2ba21282ef8d397ca5681
BorisLaevsky <borislaevsky@gmail.com>
2017-05-18 02:08:32 +0300

    Stack first try.

---
+++ Stack/stack.py
@@ -0,0 +1,36 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, element):
+        self.stack.append(element)
+
+    def pop(self):
+        deleted_element = self.stack[-1]
+        self.stack.remove(self.stack[-1])
+        return deleted_element
+
+    def peak(self):
+        return self.stack[-1]
+
+    def Print(self):
+        print self.stack
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if len(self.stack) == 0:
+            raise StopIteration
+        self.stack.pop()
+
+s = Stack()
+s.Print()
+s.push(1)
+s.push(2)
+s.push(3)
+s.Print()
+for element in s:
+    print element
+s.Print()
