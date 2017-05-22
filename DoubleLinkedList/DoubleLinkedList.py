5c1b779428f8e6d3131671bf2b5c423a9ccbb553
BorisLaevsky <borislaevsky@gmail.com>
2017-05-23 02:20:13 +0300

    now nodes with common elements connect thru constructor w

--- DoubleLinkedList/DoubleLinkedList.py
+++ DoubleLinkedList/DoubleLinkedList.py
@@ -4,6 +4,8 @@ class Node:
         self.previous_element = previous_element
         self.value = value
         self.next_element = next_element
+        if isinstance(previous_element, Node):
+           previous_element.set_next_element(self)
 
     def value(self):
         return self.value
@@ -25,8 +27,20 @@ class Node:
     def __str__(self):
         return str(self.value)
 
+#x = Node(None, 1, None)
+#y = Node(None, 2, None)
+#x.set_next_element(y)
+#x.set_previous_element(Node(None, -1, None))
+#print x.Next_element().Previous_element().Next_element()
+
+#x = Node(None, 1, None)
+#x = Node(None, 1, Node(x, 2, None))
+#x = Node(Node(None, -1, x), 1, Node(x, 2, None))
+#x = Node(Node(None, -1, x), 1, Node(x, 2, None))
+#print x.Next_element().Previous_element().Next_element()
+
 x = Node(None, 1, None)
-y = Node(None, 2, None)
-x.set_next_element(y)
-x.set_previous_element(Node(None, -1, None))
-print x.Next_element().Previous_element().Previous_element()
+y = Node(x, 2, None)
+print x.Next_element()
+print y.Previous_element()
+
