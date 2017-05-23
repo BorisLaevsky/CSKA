251c6ab247614b04a41fe77f5bc19c2ca3960655
BorisLaevsky <borislaevsky@gmail.com>
2017-05-23 16:50:50 +0300

    added private variables, removed unnecesary set_element methods

--- DoubleLinkedList/DoubleLinkedList.py
+++ DoubleLinkedList/DoubleLinkedList.py
@@ -1,36 +1,28 @@
 class Node:
 
-    def __init__(self, previous_element, value, next_element):
-        self.previous_element = previous_element
-        self.value = value
-        self.next_element = next_element
+    def __init__(self, value, previous_element = None, next_element = None):
+        self.__previous_element = previous_element
+        self.__value = value
+        self.__next_element = next_element
         if isinstance(previous_element, Node):
-           previous_element.set_next_element(self)
+           previous_element.__next_element = self
+           self.__previous_element = previous_element
         if isinstance(next_element, Node):
-           next_element.set_previous_element(self)
+           next_element.__previous_element = self
+           self.__next_element = next_element
 
     def value(self):
-        return self.value
+        return self.__value
 
-    def Next_element(self):
-        return self.next_element
+    def next_element(self):
+        return self.__next_element
 
-    def Previous_element(self):
-        return self.previous_element
-
-    def set_next_element(self, element):
-        self.next_element = element
-        element.previous_element= self
-
-    def set_previous_element(self, element):
-        self.previous_element = element
-        element.next_element = self
+    def previous_element(self):
+        return self.__previous_element
 
     def __str__(self):
-        return str(self.value)
+        return str(self.__value)
 
-x = Node(None, 1, None)
-y = Node(None, 2, x)
-print x.Next_element()
-print y.Next_element()
-
+x = Node(1,)
+y = Node(3, None, x)
+print x.previous_element()
