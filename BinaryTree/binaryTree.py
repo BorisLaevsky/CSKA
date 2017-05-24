cb72aee22b59445863cb94a2f31ee9435da2213f
BorisLaevsky <borislaevsky@gmail.com>
2017-05-24 18:48:27 +0300

    added key

--- BinaryTree/binaryTree.py
+++ BinaryTree/binaryTree.py
@@ -1,14 +1,14 @@
 class Child:
-    def __init__(self, value, parent = None, child_orientation = None, left_child = None, right_child = None):
+    def __init__(self, value, key, parent = None, left_child = None, right_child = None):
         self.__value = value
         self.__parent = parent
-        self.__child_orientation = child_orientation
+        self.__key = key
         self.__left_child = left_child
         self.__right_child = right_child
         if isinstance(parent, Child):
-           if self.__child_orientation == "left":
+           if self.__key < parent.__key:
               parent.__left_child = self
-           if self.__child_orientation == "right":
+           else:
               parent.__right_child = self
         if isinstance(left_child, Child):
            left_child.__parent = self
@@ -30,8 +30,15 @@ class Child:
     def bottom_right(self):
         return self.__right_child
 
-top = Child(1)
-left = Child(2, top, "left")
-right = Child(3, top, "right")
+    def get_key(self):
+        return self.__key
+
+    def parent(self):
+        return self.__parent
+
+top = Child(1,1)
+left = Child(2, 0, top)
+right = Child(3, 2, top)
 print top.bottom_left()
 print top.bottom_right()
+
