7dc7a23058027c7ef13c9cbb1c26f977b8053d99
BorisLaevsky <borislaevsky@gmail.com>
2017-05-26 02:32:49 +0300

    find works, REPLACE METHOD NOT COMPLETED AND NOT WORKING YET

--- BinaryTree/binaryTree.py
+++ BinaryTree/binaryTree.py
@@ -6,7 +6,6 @@ class Child:
         self.__left_child = left_child
         self.__right_child = right_child
 
-
     def __str__(self):
         return str(self.__value)
 
@@ -22,7 +21,7 @@ class Child:
     def bottom_right(self):
         return self.__right_child
 
-    def get_key(self):
+    def key(self):
         return self.__key
 
     def insert(self, value, key):
@@ -37,7 +36,33 @@ class Child:
            else:
               self.__left_child = Child(value, key, self)
 
+    def find(self, key):
+        if key != self.__key:
+            if self.__key < key:
+               return self.__right_child.find(key)
+            else:
+               return self.__left_child.find(key)
+        else:
+            return self
+
+    def remove(self, key):
+        if self.find(key).__right_child == None and self.find(key).__left_child == None:
+           self.find(key) = None
+        elif isinstance(self.find(key).__right_child, Child) and self.find(key).__left_child == None:
+           replacement = self.find(key).__right_child
+           self.find(key) = None
+           self.insert(replacement)
+        elif isinstance(self.find(key).__left_child, Child) and self.find(key).__right_child == None:
+           replacement = self.find(key).__left_child
+           self.find(key) = None
+           self.insert(replacement)
+        else:
+           self.find(key)
+
+
+
+
 top = Child(1,1)
 top.insert(2,2)
 top.insert(3,3)
-print top.bottom_right().bottom_right().parent()
+print top.bottom_right().bottom_right().value()
