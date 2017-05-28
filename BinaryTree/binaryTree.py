af242eace759c6caebe979a8ff5cda63643950ac
BorisLaevsky <borislaevsky@gmail.com>
2017-05-28 22:13:30 +0300

    replace() works

--- BinaryTree/binaryTree.py
+++ BinaryTree/binaryTree.py
@@ -43,24 +43,56 @@ class Child:
             else:
                return self.__left_child.find(key)
         else:
-            return self
+            return self.value()
+
+    def insert_child(self, child):
+        if child == None:
+           return self
+        if self.__key <= child.__key:
+           if isinstance(self.__right_child, Child):
+              self.__right_child.insert_child(child)
+           else:
+              self.__right_child = child
+              child.__parent = self
+        else:
+           if isinstance(self.__left_child, Child):
+              self.__left_child.insert_child(child)
+           else:
+              self.__left_child = child
+              child.__parent = self
+
 
     def remove(self, key):
-        removed_element = self.find(key)
-        self.find(key).__key = None
-        if isinstance(removed_element.__left_child, Child) and removed_element.__right_child == False:
-           self.insert(removed_element.__left_child)
-        if isinstance(removed_element.__right_child, Child) and removed_element.__left_child == False:
-           self.insert(removed_element.__right_child)
-        if isinstance(removed_element.__left_child, Child) and isinstance(removed_element.__right_child):
-           self.insert(removed_element.__left_child)
-           self.insert(removed_element.__right_child)
+        left_child_replacement = None
+        right_child_replacement = None
+        if isinstance(self.__left_child, Child) and key < self.__key:
+           if self.__left_child.__key != key:
+              self.__left_child.remove(key)
+        if isinstance(self.__right_child, Child) and key > self.__key:
+           if self.__right_child.__key != key:
+              self.__right_child.remove(key)
+        if isinstance(self.__right_child, Child) and  self.__right_child.__key == key:
+           if isinstance(self.__right_child.__right_child, Child):
+              right_child_replacement = self.__right_child.__right_child
+           if isinstance(self.__right_child.__left_child, Child):
+              left_child_replacement = self.__right_child.__left_child
+           self.__right_child = None
+           self.insert_child(left_child_replacement)
+           self.insert_child(right_child_replacement)
+        if isinstance(self.__left_child, Child) and  self.__left_child.__key == key:
+           if isinstance(self.__left_child.__right_child, Child):
+              right_child_replacement = self.__left_child.__right_child
+           if isinstance(self.__left_child.__left_child, Child):
+              left_child_replacement = self.__left_child.__left_child
+           self.__left_child = None
+           self.insert_child(left_child_replacement)
+           self.insert_child(right_child_replacement)
 
 
-
-top = Child(1,1)
-top.insert(2,2)
-top.insert(3,3)
-print top.bottom_right().bottom_right().value()
-top.remove(2)
-print top.bottom_right()
+top = Child(1,5)
+top.insert(2,4)
+top.insert(3,9)
+top.insert(4,10)
+top.insert(5,3)
+top.remove(4)
+print top.bottom_left()
