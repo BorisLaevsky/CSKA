d5b08440d4cd43b939dbf3d2e9a56bf89499216b
BorisLaevsky <borislaevsky@gmail.com>
2017-05-29 19:30:39 +0300

    now root can be deleted

--- BinaryTree/binaryTree.py
+++ BinaryTree/binaryTree.py
@@ -47,7 +47,7 @@ class Child:
 
     def insert_child(self, child):
         if child == None:
-           return self
+           return
         if self.__key <= child.__key:
            if isinstance(self.__right_child, Child):
               self.__right_child.insert_child(child)
@@ -61,38 +61,59 @@ class Child:
               self.__left_child = child
               child.__parent = self
 
-
-    def remove(self, key):
+    def __normal_remove(self, key):
         left_child_replacement = None
         right_child_replacement = None
         if isinstance(self.__left_child, Child) and key < self.__key:
            if self.__left_child.__key != key:
-              self.__left_child.remove(key)
-        if isinstance(self.__right_child, Child) and key > self.__key:
+              self.__left_child.__normal_remove(key)
+        elif isinstance(self.__right_child, Child) and key > self.__key:
            if self.__right_child.__key != key:
-              self.__right_child.remove(key)
+              self.__right_child.__normal_remove(key)
         if isinstance(self.__right_child, Child) and  self.__right_child.__key == key:
            if isinstance(self.__right_child.__right_child, Child):
               right_child_replacement = self.__right_child.__right_child
-           if isinstance(self.__right_child.__left_child, Child):
+           elif isinstance(self.__right_child.__left_child, Child):
               left_child_replacement = self.__right_child.__left_child
            self.__right_child = None
            self.insert_child(left_child_replacement)
            self.insert_child(right_child_replacement)
-        if isinstance(self.__left_child, Child) and  self.__left_child.__key == key:
+        elif isinstance(self.__left_child, Child) and  self.__left_child.__key == key:
            if isinstance(self.__left_child.__right_child, Child):
               right_child_replacement = self.__left_child.__right_child
-           if isinstance(self.__left_child.__left_child, Child):
+           elif isinstance(self.__left_child.__left_child, Child):
               left_child_replacement = self.__left_child.__left_child
            self.__left_child = None
            self.insert_child(left_child_replacement)
            self.insert_child(right_child_replacement)
 
+    def __corner_case(self):
+        if isinstance(self.__left_child, Child):
+           right_child = self.__right_child
+           self.__right_child = None
+           self.__key = self.__left_child.__key
+           self.__value = self.__left_child.__value
+           self.insert_child(self.__left_child.__left_child)
+           self.insert_child(self.__left_child.__right_child)
+           self.insert_child(right_child)
+        elif isinstance(self.__right_child, Child):
+           left_child = self.__left_child
+           self.__right_child = None
+           self.__key = self.__right_child.__key
+           self.__value = self.__right_child.__value
+           self.insert_child(self.__right_child.__left_child)
+           self.insert_child(self.__right_child.__right_child)
+           self.insert_child(left_child)
+    def remove(self, key_to_remove):
+        if self.__key == key_to_remove:
+           self.__corner_case()
+        else:
+           self.__normal_remove(key_to_remove)
 
 top = Child(1,5)
 top.insert(2,4)
 top.insert(3,9)
 top.insert(4,10)
 top.insert(5,3)
-top.remove(4)
-print top.bottom_left()
+top.remove(5)
+print top
