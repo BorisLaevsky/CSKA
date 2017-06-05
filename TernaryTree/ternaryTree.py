bd093ab29db2120b75b74f22927684faa54784f9
BorisLaevsky <borislaevsky@gmail.com>
2017-06-06 00:30:46 +0300

    knary tree problem with insert

--- TernaryTree/ternaryTree.py
+++ TernaryTree/ternaryTree.py
@@ -24,14 +24,11 @@ class Child:
         for child in list_of_children:
             if child == None:
                  count_of_empty_children += 1
-        if count_of_empty_children == 0:
-            return True
-        else:
-            return False
+        return count_of_empty_children == 0
 
     def insert(self, value, key_to_insert):
         if self.is_there_no_insert_space():
-            self.__left_child.insert(value, key_to_insert)
+           return self.__left_child.insert(value, key_to_insert)
         if self.__left_child == None and self.__middle_child == None and self.__right_child == None:
             self.__right_child = Child(value, key_to_insert, self)
         if self.__left_child == None and self.__middle_child == None and self.__right_child != None:
@@ -81,9 +78,20 @@ class Child:
                 self.__middle_child = self.__right_child
                 self.__right_child = Child(value, key_to_insert, self)
 
+    def find(self, key_to_find):
+        if self.__key != key_to_find:
+            if self.__left_child != None:
+                if self.__left_child.__key <= key_to_find:
+                    return self.__left_child.find(key_to_find)
+                elif self.__middle_child != None and self.__middle_child.__key <= key_to_find:
+                    return self.__middle_child.find(key_to_find)
+                elif self.__right_child != None and self.__right_child.__key <= key_to_find:
+                    return self.__right_child.find(key_to_find)
+
 
 
 
 top = Child(1,1)
 top.insert(2,2)
 print top.right_child()
+top.find(2)
---
+++ knaryTree/knaryTree.py
@@ -0,0 +1,55 @@
+class Child:
+    def __init__(self, value, number_of_children, parent = None, list_of_children = []):
+        self.__value = value
+        self.__parent = parent
+        self.__number_of_children = number_of_children
+        self.__list_of_children = list_of_children
+
+    def __str__(self):
+        return str(self.__value)
+
+    def parent(self):
+        return self.__parent
+
+    def list_of_children(self):
+        return self.__list_of_children
+
+    def get_kth_child(self, number_of_child):
+        try:
+            return self.__list_of_children[number_of_child - 1]
+        except IndexError:
+            return "Child does not exist"
+
+    def insert(self, value_to_insert, number_of_grandchildren):
+        if len(self.__list_of_children) < self.__number_of_children:
+            self.__list_of_children.append(Child(value_to_insert, number_of_grandchildren, self))
+        else:
+            for child in self.__list_of_children:
+                print len(child.__list_of_children)
+                if len(child.__list_of_children) < child.__number_of_children:
+                    print "inserted"
+                    child.__list_of_children.append(Child(value_to_insert, number_of_grandchildren, child))
+                    break
+                else:
+                    child.__list_of_children[0].insert(value_to_insert, number_of_grandchildren)
+
+    def depth_first_search(self, value_to_find):
+        result = 0
+        for child in self.__list_of_children:
+            if child.__value == value_to_find:
+                result = child
+                return result
+            else:
+                if child.__list_of_children != []:
+                    child.depth_first_search(value_to_find)
+
+    def broadth_first_search(self, value_to_find):
+        result = 0
+
+
+
+root = Child(1,3)
+root.insert(2,3)
+root.insert(3,3)
+root.insert(4,3)
+root.insert(5,3)
